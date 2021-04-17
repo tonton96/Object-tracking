45f11490ec03b5f418f16a9e075fce0c4fb70daf
vinhlc <lcv0611@gmail.com>
2021-04-17 14:34:21 +0700

    [vinhlc] update ui

---
+++ yolov4_deepsort/my_ui.py
@@ -0,0 +1,15 @@
+from tkinter import*
+from tkinter import filedialog as fd
+import my_object_tracker as obt
+
+def openFile():
+    name = fd.askopenfilename()
+    tracker = obt.MyTracker(name)
+    tracker.tracking()
+
+root = Tk()
+root.title("Object tracking")
+root.geometry("400x200")
+openFileBtn = Button(root, text = "Open file",width =100, height=100, command=openFile)
+openFileBtn.pack()
+root.mainloop()
